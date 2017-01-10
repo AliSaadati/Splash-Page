3da071a9731fc1fe93083c10f6459301980482e1
Ali Saadati <AliSaadati619@yahoo.com>
2017-01-10 15:46:53 -0800

    Added Content to HTML page

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
     "use strict";
     dropdownmenu = document.querySelector(".dropdown");
     var strokes = document.querySelectorAll(".strokes")
-        , icon = document.querySelector(".menu")
-        , dropdownmenu = document.querySelector(".dropdown");
+        , icon = document.querySelector(".hamburger")
+        , dropdownmenu = document.querySelector(".dropdown")
+        , dropdownResume = document.querySelector("#dropdown-r");
     var mq = window.matchMedia("(min-width: 720px)");
 
     function transformStart() {
+        if (dropdownmenu.classList.contains("show")) {
+            dropdownResume.nextElementSibling.classList.remove("show-rsm");
+        }
         strokes[0].classList.toggle("animate0");
         strokes[1].classList.toggle("hide");
         strokes[2].classList.toggle("animate2");
@@ -16,11 +20,17 @@
     function menuCheck() {
         if (mq.matches) {
             dropdownmenu.classList.remove("show");
-            strokes[0].classList.remove("animate0")
+            dropdownResume.nextElementSibling.classList.remove("show-rsm");
+            strokes[0].classList.remove("animate0");
             strokes[1].classList.remove("hide");
             strokes[2].classList.remove("animate2");
         }
     }
+
+    function dropdownResumeCheck() {
+        dropdownResume.nextElementSibling.classList.toggle("show-rsm");
+    }
     window.addEventListener("resize", menuCheck);
     icon.addEventListener("click", transformStart);
+    dropdownResume.addEventListener("click", dropdownResumeCheck);
 })();
